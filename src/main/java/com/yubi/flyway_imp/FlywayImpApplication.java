b31cd7913c3c3b5c4bcade6d77cb8d6e316de9fb
Chandan Thakur <chandan.thakur1@go-yubi.com>
2024-06-04 11:32:32 +0530

    first commit

---
+++ src/main/java/com/yubi/flyway_imp/FlywayImpApplication.java
@@ -0,0 +1,13 @@
+package com.yubi.flyway_imp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlywayImpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlywayImpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yubi/flyway_imp/FlywayImpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yubi.flyway_imp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlywayImpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
